9fdbaf543b6f885a1454a13aa25b252cc9ce00bb
na080411-star <na080411@gmail.com>
2025-08-18 00:52:47 +0900

    Update route.js

--- app/api/news/route.js
+++ app/api/news/route.js
@@ -5,11 +5,37 @@ const parser = new Parser();
 
 const newsSources = [
   { name: 'BBC News', url: 'http://feeds.bbci.co.uk/news/rss.xml' },
-  { name: 'The Verge', url: 'https://www.theverge.com/rss/index.xml' },
+  { name: 'BBC Sport', url: 'http://feeds.bbci.co.uk/sport/rss.xml' },
+  { name: 'CNN World', url: 'http://rss.cnn.com/rss/edition_world.rss' },
   { name: 'The Guardian', url: 'https://www.theguardian.com/world/rss' },
-  { name: 'The New York Times World', url: 'https://rss.nytimes.com/services/xml/rss/nyt/World.xml' },
-  { name: 'The New York Times Technology', url: 'https://rss.nytimes.com/services/xml/rss/nyt/Technology.xml' }
+  { name: 'The Verge', url: 'https://www.theverge.com/rss/index.xml' },
+  { name: 'The New York Times - World', url: 'https://rss.nytimes.com/services/xml/rss/nyt/World.xml' },
+  { name: 'The New York Times - Technology', url: 'https://rss.nytimes.com/services/xml/rss/nyt/Technology.xml' },
+  { name: 'Google News', url: 'https://news.google.com/rss' },
+  { name: 'Ars Technica', url: 'http://feeds.arstechnica.com/arstechnica/index' },
+  { name: 'TechCrunch', url: 'http://feeds.feedburner.com/TechCrunch' },
+  { name: 'Hacker News', url: 'https://news.ycombinator.com/rss' },
+  { name: 'Lifehacker', url: 'https://lifehacker.com/rss' },
+  { name: 'Forbes Business', url: 'https://www.forbes.com/business/feed/' },
+  { name: 'Fortune', url: 'https://fortune.com/feed' },
+  { name: 'CNBC Top News', url: 'https://www.cnbc.com/id/100003114/device/rss/rss.html' },
+  { name: 'Scientific American', url: 'http://rss.sciam.com/ScientificAmerican-Global' },
+  { name: 'Nature', url: 'https://www.nature.com/nature.rss' },
+  { name: 'ScienceDaily', url: 'https://www.sciencedaily.com/rss/all.xml' },
+  { name: 'NASA Breaking News', url: 'https://www.nasa.gov/rss/dyn/breaking_news.rss' },
+  { name: 'NPR Hidden Brain', url: 'https://feeds.npr.org/510308/podcast.xml' },
+  { name: 'The Onion', url: 'https://www.theonion.com/rss' },
+  { name: 'xkcd', url: 'https://xkcd.com/rss.xml' },
+  { name: 'Slashfilm', url: 'https://feeds2.feedburner.com/slashfilm' },
+  { name: 'Variety', url: 'https://variety.com/feed/' },
+  { name: 'Smitten Kitchen', url: 'http://feeds.feedburner.com/smittenkitchen' },
+  { name: 'Serious Eats - Recipes', url: 'http://feeds.feedburner.com/seriouseats/recipes' },
+  { name: 'The Kitchn', url: 'https://www.thekitchn.com/main.rss' },
+  { name: 'Stratechery', url: 'http://stratechery.com/feed/' },
+  { name: 'Slashdot', url: 'http://rss.slashdot.org/Slashdot/slashdotMain' },
+  { name: 'Product Hunt', url: 'https://www.producthunt.com/feed' }
 ];
+
 
 export async function GET() {
   const allNews = [];
